b1bf65a23c83174e036a3b07f5aa3e791d6b7b2c
Bratah123 <brandonnguyen3301@gmail.com>
2022-02-23 19:05:30 -0800

    printing out inputs for assignment 1

--- week_2/addition_calculator/run.sh
+++ week_2/addition_calculator/run.sh
@@ -3,6 +3,10 @@
 #Program: Addition Calculator Compilation Script
 #Author: Brandon Nguyen
 
-echo "Bash: The script file for Hello World has begun"
+echo "Bash: The script file for Addition Calculator has begun"
 
-g++ -c -m64 -Wall -fno-pie -no-pie -std=c++17 -o scan.o addition_calculator.cpp && nasm -f elf64 -o valid.o calculator.asm && g++ -m64 -fno-pie -no-pie -o code.out -std=c++17 scan.o valid.o
+FULL_COMMAND="g++ -c -m64 -Wall -fno-pie -no-pie -std=c++17 -o scan.o addition_calculator.cpp && nasm -f elf64 -o valid.o calculator.asm && g++ -m64 -fno-pie -no-pie -o code.out -std=c++17 scan.o valid.o"
+
+eval "$FULL_COMMAND"
+
+echo "Successfully compiled the program!"
